8308736c6105eb760e7a34506c065576be55e25c
Elaine Yu <elaine@monsoonco.com>
2015-06-10 14:31:48 -0700

    filters...return to this subject

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,6 +2,8 @@
 
 /* Filters */
 
+// Added as a dependency in app.js, app module global
+// include  js/filters.js as script in template.
 angular.module('phonecatFilters', []).filter('checkmark', function() {
   return function(input) {
     return input ? '\u2713' : '\u2718';
